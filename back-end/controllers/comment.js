c5307735cc079b3f5bd1ed2fa444408d7175dae2
ngoclias <ngoc.leanh1999@gmail.com>
2024-07-17 07:32:51 +0700

    feat(BE): update, delete comment

--- back-end/controllers/comment.js
+++ back-end/controllers/comment.js
@@ -3,12 +3,42 @@ const Post = require("../models/Post");
 
 exports.createComment = async (req, res) => {
   try {
-    const comment = new Comment({ ...req.body, commentBy: req.user.id });
+    const comment = new Comment({ ...req.body, commentBy: req.user._id });
     await comment.save();
 
     const post = await Post.findById(req.body.post);
     post.comments = [...post.comments, comment._id];
     await post.save();
+
+    return res.json({ message: "ok" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+exports.updateComment = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const comment = await Comment.findById(req.body.id);
+
+    if (!comment) {
+      return res.status(400).json({ message: "Comment not found." });
+    }
+
+    if (text) comment.text = text;
+    if (image) comment.image = image;
+
+    await comment.save();
+
+    return res.json({ message: "ok" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+};
+
+exports.deleteComment = async (req, res) => {
+  try {
+    await Comment.findByIdAndDelete(req.params.id);
 
     return res.json({ message: "ok" });
   } catch (err) {
--- back-end/routes/comment.js
+++ back-end/routes/comment.js
@@ -5,5 +5,7 @@ const { authUser } = require("../middlewares/auth");
 const router = express.Router();
 
 router.post("/createComment", authUser, commentController.createComment);
+router.post("/updateComment", authUser, commentController.updateComment);
+router.delete("/deleteComment/:id", authUser, commentController.deleteComment);
 
 module.exports = router;
