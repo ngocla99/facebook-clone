2270272bc67ccad4de8333dd4df5cda24b132d63
ngocla.99 <ngoc.leanh1999@gmail.com>
2024-08-09 17:02:24 +0700

    feat(FE): update UI friend page

--- front-end/src/pages/friends/friend-home/index.jsx
+++ front-end/src/pages/friends/friend-home/index.jsx
@@ -6,22 +6,77 @@ export const FriendHome = () => {
   return (
     <FriendsLayout>
       <div className="grid p-2">
-        <div className="">
-          <h3 className='text-[24px]'>Friends</h3>
-          <i className="settings_filled_icon"></i>
+        <div className="flex justify-between px-2 pt-1">
+          <h3 className="text-[24px] font-bold">Friends</h3>
+          <Button variant="secondary" size="icon" className="size-9">
+            <i className="settings_filled_icon"></i>
+          </Button>
         </div>
-        <div className="grid">
-          <i className="friends_home_icon"></i>
-          <Button variant="ghost" className="justify-start flex">
+        <div className="mt-2 grid">
+          <Button
+            variant="ghost"
+            size="xl"
+            className="flex justify-start gap-3 px-2 text-lg"
+          >
+            <div className="grid size-9 place-items-center rounded-full bg-background-secondary">
+              <i className="friends_home_icon"></i>
+            </div>
             Home
           </Button>
-          <i className="right_icon"></i>
-
-          <i className="friends_requests_icon"></i>
-          <i className="friends_suggestions_icon"></i>
-          <i className="all_friends_icon"></i>
-          <i className="birthdays_icon"></i>
-          <i className="all_friends_icon"></i>
+          <Button
+            variant="ghost"
+            size="xl"
+            className="flex justify-start gap-3 px-2 text-lg"
+          >
+            <div className="grid size-9 place-items-center rounded-full bg-background-secondary">
+              <i className="friends_requests_icon"></i>
+            </div>
+            Friend Requests
+            <i className="right_icon ml-auto"></i>
+          </Button>
+          <Button
+            variant="ghost"
+            size="xl"
+            className="flex justify-start gap-3 px-2 text-lg"
+          >
+            <div className="grid size-9 place-items-center rounded-full bg-background-secondary">
+              <i className="friends_suggestions_icon"></i>
+            </div>
+            Suggestions
+            <i className="right_icon ml-auto"></i>
+          </Button>
+          <Button
+            variant="ghost"
+            size="xl"
+            className="flex justify-start gap-3 px-2 text-lg"
+          >
+            <div className="grid size-9 place-items-center rounded-full bg-background-secondary">
+              <i className="all_friends_icon"></i>
+            </div>
+            All friends
+            <i className="right_icon ml-auto"></i>
+          </Button>
+          <Button
+            variant="ghost"
+            size="xl"
+            className="flex justify-start gap-3 px-2 text-lg"
+          >
+            <div className="grid size-9 place-items-center rounded-full bg-background-secondary">
+              <i className="birthdays_icon"></i>
+            </div>
+            Birth days
+          </Button>
+          <Button
+            variant="ghost"
+            size="xl"
+            className="flex justify-start gap-3 px-2 text-lg"
+          >
+            <div className="grid size-9 place-items-center rounded-full bg-background-secondary">
+              <i className="all_friends_icon"></i>
+            </div>
+            Custom Lists
+            <i className="right_icon ml-auto"></i>
+          </Button>
         </div>
       </div>
       <div>Right</div>
